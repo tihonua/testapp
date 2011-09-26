8c87739f301326bc4e38dd624736a1637b6066f8
sergey.tischenko@quadecco.com <sergey.tischenko@quadecco.com>
2011-09-26 12:44:12 +0300

    empty rails project

---
+++ emptyrails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Emptyrails::Application.config.secret_token = 'e90471e5e6dbfc1403c6332f0e9aaf1b8dd93d89cc56c018a402ed42524744c2842da43aaccb0f9adef9f5a7b58165c064ecd05fece25caae4683bfe0ce70d43'
